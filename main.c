66dcbaa8edae655736aabf5741606347f2886900
abrasax <whoismath@gmail.com>
2019-08-25 18:46:41 -0300

    also need to change main.c

--- main.c
+++ main.c
@@ -12,23 +12,26 @@ int main(int argc, char **argv) {
     size_t len = 32;
     long long int tentativa = 0;
 
-    uint8_t hash[32], answer[32];
+    uint8_t answer[32];
     uint8_t challenge[33];
+    uint8_t rand[32];
 
     if (argc < 3) {
-        printf("usage: %s <message> <seed> <difficulty>", argv[0]);
+        printf("usage: %s <seed> <difficulty>", argv[0]);
         return 0;
     }
 
-    sha3(argv[1], strlen(argv[1]), hash, len);
-    pow_initrand(argv[2]);
+    // pow_initrand(argv[1]);
+    memcpy(rand, argv[1], sizeof(rand));
 
-    pow_gen(atoi(argv[3]), challenge);
+    pow_gen(atoi(argv[2]), challenge, rand);
     printf("[CHALLENGE] SHA3-%lu:\n", len);
     for(int i=0; i < len; i++) printf("%02X", challenge[i]);
     printf("\nDificuldade: %d\n", challenge[32]);
 
-    while (!pow_try(challenge, answer)) {
+    while (!pow_try(challenge, answer, rand)) {
+        for(int i=0; i < len; i++) printf("%02X", rand[i]);
+        printf("\n");
         tentativa++;
     }
     printf("Tentativas: %lli\n", tentativa);
